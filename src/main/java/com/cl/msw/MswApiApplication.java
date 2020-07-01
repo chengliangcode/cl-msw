d70d28945d06a6e60e354ce6f92d2c5eca084662
akacl <714552682@qq.com>
2020-07-01 10:43:14 +0800

    Initial commit

---
+++ src/main/java/com/cl/msw/MswApiApplication.java
@@ -0,0 +1,19 @@
+package com.cl.msw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动
+ *
+ * @author chengliang
+ * @date 2020/7/1 10:32
+ */
+@SpringBootApplication
+public class MswApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MswApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cl/msw/MswApiApplicationTests.java
@@ -0,0 +1,20 @@
+package com.cl.msw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 单元测试
+ *
+ * @author chengliang
+ * @date 2020/7/1 10:35
+ */
+@SpringBootTest
+public class MswApiApplicationTests {
+
+    @Test
+    void context() {
+        System.out.println("my junit test");
+    }
+
+}
