5bd6df50042aee228bfbbc49c27fa146701f4a72
akacl <714552682@qq.com>
2021-01-05 15:09:53 +0800

    init

---
+++ cl-msw-core/src/main/java/com/cl/code/MswMain.java
@@ -0,0 +1,17 @@
+package com.cl.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chengliang
+ * @date 2021/1/5 14:54
+ */
+@SpringBootApplication
+public class MswMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MswMain.class, args);
+    }
+    
+}
---
+++ cl-security/src/main/java/com/cl/code/security/SecurityMain.java
@@ -0,0 +1,17 @@
+package com.cl.code.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chengliang
+ * @date 2021/1/5 14:46
+ */
+@SpringBootApplication
+public class SecurityMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityMain.class, args);
+    }
+
+}
